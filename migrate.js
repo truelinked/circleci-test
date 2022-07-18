8f8b055a05a2dcad52733d34083ad53a29416687
Tummala Dhanvi <tud@artsconsolidated.com>
2022-07-18 17:37:57 +0530

    ci: update task definition without id

--- migrate.js
+++ migrate.js
@@ -1,5 +1,5 @@
 async function migrate() {
-    for (var i = 0; i < 300; i++) {
+    for (var i = 0; i < 30; i++) {
         console.log(`Migrating table ${i + 1}`)
         await new Promise(resolve => setTimeout(resolve, 1000));
     }
